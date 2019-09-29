0093aa5a0d87b51af5014149953a1c5d0eb81c2a
lsxuyimin <lsxuyimin@hotmail.com>
2019-09-30 02:01:28 +0800

    2
    2

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -13,13 +13,19 @@ import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.NDArrayFactory;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.learning.config.Nadam;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Console;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
@@ -34,8 +40,14 @@ public class Main {
         int batchSize = 64; // batch size for each epoch
         int rngSeed = 1023; // random number seed for reproducibility
         int numEpochs = 1; // number of epochs to perform
-        double rate = 0.015; // learning rate
+        double rate = 0.15; // learning rate
 
+        INDArray t= Nd4j.zeros(10,10).addi(0);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                t.putScalar(i,j,i*10+j);
+            }
+        }
         //Get the DataSetIterators:
         DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
         DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
@@ -44,24 +56,33 @@ public class Main {
         log.info("Build model....");
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .seed(rngSeed) //include a random seed for reproducibility
-                .activation(Activation.RELU)
+                .activation(Activation.SIGMOID)
                 .weightInit(WeightInit.XAVIER)
                 .updater(new Nadam())
                 .l2(rate * 0.005) // regularize learning model
                 .list()
                 .layer(new DenseLayer.Builder() //create the first input layer.
                         .nIn(numRows * numColumns)
-                        .nOut(500)
+                        .nOut(1000)
                         .build())
-                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.SQUARED_LOSS) //create hidden layer
-                        .activation(Activation.SOFTMAX)
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.MSE) //create hidden layer
+                        .activation(Activation.SIGMOID)
                         .nOut(outputNum)
                         .build())
                 .build();
+        MultiLayerNetwork model;
+        try {
+            File f = new File("01.mdl");
+            model = MultiLayerNetwork.load(f, false);
+            System.out.println("Load model.... ");
+        }
+        catch (FileNotFoundException fnfe) {
+            model = new MultiLayerNetwork(conf);
+            model.init();
+            model.save(new File("0.mdl"));
+            System.out.println("Save model.... ");
+        }
 
-        MultiLayerNetwork model = new MultiLayerNetwork(conf);
-        model.init();
-        model.setListeners(new ScoreIterationListener(500));  //print the score with every iteration
         System.out.println("Train model....");
 
         double b0=0;
@@ -70,11 +91,10 @@ public class Main {
             //model.getLayer(0).getParam("b").putScalar(0, 0,b0);
             model.fit(mnistTrain, numEpochs);
 
-            System.out.println("Evaluate model....");
+            System.out.println("Evaluate model.... "+i);
             Evaluation eval = model.evaluate(mnistTest);
 
             System.out.println(eval.accuracy());
-            Console.println("****************Example finished********************");
         }
 
     }
