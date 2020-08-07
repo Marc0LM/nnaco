db7f82136bd2a07ba5d901e4f2f4126006010891
lsxuyimin <lsxuyimin@hotmail.com>
2020-08-07 20:03:38 +0800

    plot

--- plott.m
+++ plott.m
@@ -3,8 +3,8 @@
 %m=csvread('MPGTestaco-bp 218 52169937.csv');
 m=csvread('Last.csv');
 s=size(m);
-it=10;
-numExp=12;
+it=5;
+numExp=4;
 t=downsample(m(1,:),it);
 b=zeros(numExp,s(2)/it);
 ab=zeros(numExp,s(2)/it);
@@ -15,7 +15,7 @@ for i=1:numExp
     hab(i,:)=downsample(mean(m(8*numExp+1+(i-1)*4+1:8*numExp+4+(i-1)*4+1,:)),it); 
 end
 figure(1);
-plot(t(1,:),ab(1,:),':k',t(1,:),b(1,:),'-k',t(1,:),hab(1,:),'--k');
+plot(t(1,:),ab(4,:),':k',t(1,:),b(4,:),'-k',t(1,:),hab(4,:),'--k');
 figure(2);
-bo=[ab(1:numExp,s(2)/it),b(1:numExp,s(2)/it),hab(1:numExp,s(2)/it)];
+bo=[m(0*numExp+1+(i-1)*4+1:0*numExp+4+(i-1)*4+1,s(2)),m(4*numExp+1+(i-1)*4+1:4*numExp+4+(i-1)*4+1,s(2)),m(8*numExp+1+(i-1)*4+1:8*numExp+4+(i-1)*4+1,s(2))];
 boxplot(bo);
